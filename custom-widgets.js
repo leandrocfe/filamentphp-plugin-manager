8f056e9ec76f0fb08358ea5a3c6dd1ee8dfcdc74
Leandro Ferreira <leandrocfe@gmail.com>
2025-06-25 06:29:21 -0300

    wip

--- custom-widgets.js
+++ custom-widgets.js
@@ -1,26 +1,3 @@
 window.addEventListener('DOMContentLoaded', () => {
     CMS.init();
-    const observer = new MutationObserver(() => {
-        const docsType = document.querySelector('[name="docs_type"]');
-        const docsUrlField = document.querySelector('[name="docs_url"]')?.closest('.nc-controlPane-widget');
-        const docsUrlsField = document.querySelector('[name="docs_urls.v2"]')?.closest('.nc-controlPane-widget')?.parentElement?.parentElement;
-
-        if (docsType && docsUrlField && docsUrlsField) {
-            const updateVisibility = () => {
-                if (docsType.value === 'Single version') {
-                    docsUrlField.style.display = 'block';
-                    docsUrlsField.style.display = 'none';
-                } else {
-                    docsUrlField.style.display = 'none';
-                    docsUrlsField.style.display = 'block';
-                }
-            };
-
-            docsType.addEventListener('change', updateVisibility);
-            updateVisibility();
-            observer.disconnect();
-        }
-    });
-
-    observer.observe(document.body, { childList: true, subtree: true });
 });
