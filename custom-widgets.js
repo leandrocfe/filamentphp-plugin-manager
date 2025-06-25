c82f1bde660f74756e4906402cb7ecc43e04cff2
Leandro Ferreira <leandrocfe@gmail.com>
2025-06-25 06:35:56 -0300

    wip

--- custom-widgets.js
+++ custom-widgets.js
@@ -1,3 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
     console.log('Custom widgets initialized');
+
+    var docs_type = document.getElementById('docs_type-field-22');
+    console.log(docs_type)
 });
